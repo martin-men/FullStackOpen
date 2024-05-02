fc85f8cb023aaa4f3af5e0af604601860d6f49f8
Martin Mendieta <marmenthor@gmail.com>
2024-05-01 22:39:32 -0500

    Fetch data from server with axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddForm from "./components/AddForm";
 import Filter from "./components/Filter";
 import Person from "./components/Person";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [shownPersons, setShownPersons] = useState(persons);
+  const [shownPersons, setShownPersons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data);
+        setShownPersons(response.data);
+      });
+  }, []);
 
   const handleInputChange = (event) => {
     switch (event.target.name) {
@@ -74,12 +79,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter handleFilterChange={handleFilterChange}/>
+      <Filter handleFilterChange={handleFilterChange} />
       <h2>Add a new number</h2>
-      <AddForm handleAddPerson={handleAddPerson} newName={newName} newNumber={newNumber} handleInputChange={handleInputChange}/>
+      <AddForm
+        handleAddPerson={handleAddPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleInputChange={handleInputChange}
+      />
       <h2>Numbers</h2>
       {shownPersons.map((person) => (
-        <Person key={person.name} person={person}/>
+        <Person key={person.name} person={person} />
       ))}
     </div>
   );
